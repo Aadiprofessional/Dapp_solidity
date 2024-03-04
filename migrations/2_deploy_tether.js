668fb44040a69c28a14c253ff1ecf5c60a21efa5
Aadiprofessional <aadiprofessional8@gmail.com>
2024-03-05 04:51:13 +0530

    Third commit

--- migrations/2_deploy_tether.js
+++ migrations/2_deploy_tether.js
@@ -1,5 +1,20 @@
 const Tether = artifacts.require('Tether');
+const RWD = artifacts.require('RWD');
+const DecentralBank = artifacts.require('DecentralBank');
 
-module.exports = async function (deployer) {
+module.exports = async function (deployer, network, accounts) {
     await deployer.deploy(Tether);
+    const tether = await Tether.deployed();
+
+    await deployer.deploy(RWD);
+    const rwd = await RWD.deployed();
+
+    await deployer.deploy(DecentralBank, rwd.address, tether.address);
+    const decentralBank = await DecentralBank.deployed();
+
+    await rwd.transfer(decentralBank.address,'1000000000000000000000000')
+
+    console.log('Account to receive tokens:', accounts[1]);
+    await tether.transfer(accounts[1], '1000000000000000000');
+    
 };
